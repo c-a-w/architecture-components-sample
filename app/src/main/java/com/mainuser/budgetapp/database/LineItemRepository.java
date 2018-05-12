e676c431c87a0e70087f8321e2051dc8abae0047
Chris Whitehead <cawhitehead@gmail.com>
2018-05-12 15:05:31 +0900

    Update selected LineItem instead of creating a new one

--- app/src/main/java/com/mainuser/budgetapp/database/LineItemRepository.java
+++ app/src/main/java/com/mainuser/budgetapp/database/LineItemRepository.java
@@ -1,10 +1,13 @@
 package com.mainuser.budgetapp.database;
 
 import android.arch.lifecycle.LiveData;
+import android.util.Log;
 
 import java.util.List;
 
 public class LineItemRepository {
+    private static final String TAG = LineItemRepository.class.getSimpleName();
+
     private LineItemDao lineItemDao;
 
     public LineItemRepository(LineItemDao lineItemDao) {
@@ -20,6 +23,7 @@ public class LineItemRepository {
     }
 
     public void updateLineItem(LineItem lineItem) {
+        Log.d(TAG, lineItem.toString());
         lineItemDao.insert(lineItem);
     }
 
--- app/src/main/java/com/mainuser/budgetapp/detail/LineItemViewModel.java
+++ app/src/main/java/com/mainuser/budgetapp/detail/LineItemViewModel.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
 import com.mainuser.budgetapp.database.LineItem;
 import com.mainuser.budgetapp.database.LineItemRepository;
@@ -22,11 +23,26 @@ public class LineItemViewModel extends ViewModel {
     }
 
     LiveData<LineItem> getLineItemAt(int id) {
-        return repository.getLineItem(id);
+        LiveData<LineItem> currentLineItem = repository.getLineItem(id);
+        lineItem = currentLineItem;
+        return currentLineItem;
     }
 
-    void updateLineItem(LineItem lineItem) {
-        repository.updateLineItem(lineItem);
+    void updateLineItem(long newDate, String newDescription, double newAmount, String newCategory) {
+        if (null == lineItem) {
+            Log.d(TAG, "lineItem not set");
+            return;
+        }
+        LineItem newItem = lineItem.getValue();
+        if (null == newItem) {
+            Log.d(TAG, "No value for " + lineItem);
+            return;
+        }
+        newItem.setDate(newDate);
+        newItem.setDescription(newDescription);
+        newItem.setAmount(newAmount);
+        newItem.setCategory(newCategory);
+        repository.updateLineItem(newItem);
     }
 
 }
